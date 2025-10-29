6fc9898e0f3c25027bd400bfcbb1d4b97154d0b4
ángel huaman <75207696@ejemplo.com>
2025-10-28 22:45:26 -0500

    Primer commit: app y pruebas

---
+++ app.py
@@ -0,0 +1,6 @@
+# app.py
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Resultado:", sumar(3, 4))
---
+++ test_app.py
@@ -0,0 +1,5 @@
+# test_app.py
+from app import sumar
+
+def test_sumar():
+    assert sumar(3, 4) == 7
